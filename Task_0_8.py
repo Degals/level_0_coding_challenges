3f0d2a971a75797c537ccb853ecda39721e2e3e8
Decclan Lawrence <declan26@gmail.com>
2021-03-09 19:41:19 +0200

    Add task 0.8

---
+++ Task_0_8.py
@@ -0,0 +1,14 @@
+def time_conversion(number):
+    hour = number // 60
+    mins = (number % 60)
+    time = hour + mins
+    string_hour = str(hour)
+    string_min = str(mins)
+    if hour <= 1:
+        print(string_hour + " " + "hour")
+    else:
+        print(string_hour + " " + "hours")
+    if mins <= 1:
+        print(string_min + " " + "minute")
+    else:
+        print(string_min + " " + "minutes")
