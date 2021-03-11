54ebcb9d35ec22658429d381ccd1fe727521afac
Decclan Lawrence <declan26@gmail.com>
2021-03-11 15:41:22 +0200

    Refined Task 0.6

--- Task_0_6.py
+++ Task_0_6.py
@@ -1,28 +1,5 @@
-def maximum(int1, int2, int3):
-    if int1 > int2:
-        return int1
-    elif int1 > int3:
-        return int1
-    elif int2 > int3:
-        return int2
-    else:
-        return int3
+def maximum(*numbers):
+    return max(*numbers)
 
 
-maximum(1, 2, 3)
-
-
-def maximum_2(int1, int2, int3, int4):  # add int.. to parameter and edd elif int.. argument to function
-    if int1 > int2:
-        return int1
-    elif int1 > int3:
-        return int1
-    elif int2 > int3:
-        return int2
-    elif int4 > int3:
-        return int4
-    else:
-        return int3
-
-
-maximum_2(2, 4, 22, 88)
+maximum(200000, 44, 100, 500000000, 80000000000)
