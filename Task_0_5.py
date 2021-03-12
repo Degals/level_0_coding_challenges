2b9af152eae5315d5cea69925036be884dee109c
Decclan Lawrence <declan26@gmail.com>
2021-03-12 21:55:04 +0200

    refined Task 0.5

--- Task_0_5.py
+++ Task_0_5.py
@@ -1,6 +1,6 @@
-def area_of_a_triangle(a, b, c):
-    s = 0.5 * (a + b + c)
-    area = (s * (s - a) * (s - b) * (s - c)) ** 0.5
+def area_of_a_triangle(side_1, side_2, side_3):
+    semi_perimeter = 0.5 * (side_1 + side_2 + side_3)
+    area = (semi_perimeter * (semi_perimeter - side_1) * (semi_perimeter - side_2) * (semi_perimeter - side_3)) ** 0.5
     return area
 
 
